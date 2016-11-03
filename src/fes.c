f18026ab4a8a3656b68790b36dc6a72722e2b8a8
Briol Frédéric <fbriol@cls.fr>
2016-11-03 23:46:44 +0100

    Check the presence of the BSD library to use the functions strlcpy/strlcat for Linux.

--- src/fes.c
+++ src/fes.c
@@ -14,10 +14,14 @@
    along with FES.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <config.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+#ifdef HAVE_BSD_STRING
+#include <bsd/string.h>
+#endif
 
 #include "fes.h"
 
