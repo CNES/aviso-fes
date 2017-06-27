737342edabd5e9f4c37fbb38f94d303a6c401cd7
Briol Frédéric <fbriol@cls.fr>
2017-06-27 08:53:26 +0200

    Migration of the license of GPL to LGPL.

--- python/setup.py
+++ python/setup.py
@@ -176,7 +176,8 @@ def main():
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
-        'OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'OSI Approved :: GNU LESSER GENERAL PUBLIC LICENSE v3 or later '
+        '(LGPLv3+)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
