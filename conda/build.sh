f9778ad98bf0d8d8833779544daf081839412b75
Frédéric BRIOL <fbriol@gmail.com>
2022-08-12 14:40:44 +0200

    use pip to install pyfes.

--- conda/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-mkdir build
-cd build
-cmake .. -DBUILD_PYTHON=on -DPython3_EXECUTABLE=$PYTHON -DBUILD_SHARED_LIBS=on -DCMAKE_INSTALL_PREFIX=$PREFIX
-make -j
-make test
-make install
