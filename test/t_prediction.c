31bbb82caa6c52cb3277de5c8d3e1e650f48af7c
Frédéric BRIOL <fbriol@gmail.com>
2024-06-17 17:29:04 +0200

    Fix tide type for "MSF" wave in prediction.c

--- test/t_prediction.c
+++ test/t_prediction.c
@@ -707,7 +707,7 @@ main(void)
         err = CHECK_FLOAT(w[i].freq, 1.01589578 * RAD);
         SUMMARIZE_ERR;
 
-        err = CHECK_INT(w[i].type, SP_TIDE);
+        err = CHECK_INT(w[i].type, LP_TIDE);
         SUMMARIZE_ERR;
         break;
       case SSA:
