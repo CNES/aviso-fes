c91dcce11de45145283efcc07ef8624de922a6ec
Frédéric BRIOL <fbriol@gmail.com>
2024-06-29 13:26:45 +0200

    chore: Update files with copyright notice and brief description

--- include/fes/detail/broadcast.hpp
+++ include/fes/detail/broadcast.hpp
@@ -1,5 +1,10 @@
+// Copyright (c) 2023 CNES
+//
+// All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+/// @file include/fes/detail/broadcast.hpp
+/// @brief Broadcasting utilities.
 #pragma once
-
 #include <sstream>
 #include <stdexcept>
 #include <string>
--- include/fes/detail/isviewstream.hpp
+++ include/fes/detail/isviewstream.hpp
@@ -1,4 +1,9 @@
-
+// Copyright (c) 2023 CNES
+//
+// All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+/// @file include/fes/detail/isviewstream.hpp
+/// @brief String view stream.
 #pragma once
 #include <algorithm>
 #include <istream>
