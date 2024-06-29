0902758715851bde0a5f123452a401186ec6ffc9
Frédéric BRIOL <fbriol@gmail.com>
2024-06-29 13:26:45 +0200

    chore: Update files with copyright notice and brief description

--- include/fes/string_view.hpp
+++ include/fes/string_view.hpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2023 CNES
+//
+// All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+/// @file include/fes/string_view.hpp
+/// @brief A non-owning view of a string.
 #pragma once
 
 #include <cstddef>
