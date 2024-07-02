cdf95a0c5d813657472e5dd2b247b2ca9093cd4e
Frédéric BRIOL <fbriol@gmail.com>
2024-07-02 22:30:33 +0200

    refactor: Update Eigen aliases and add dynamic stride and reference

--- include/fes/eigen.hpp
+++ include/fes/eigen.hpp
@@ -5,7 +5,6 @@
 /// @file include/fes/eigen.hpp
 /// @brief Eigen aliases
 #pragma once
-
 #include <Eigen/Core>
 
 namespace fes {
@@ -19,5 +18,12 @@ using Vector = Eigen::Matrix<T, Eigen::Dynamic, 1>;
 /// @tparam T The type of the matrix elements.
 template <typename T>
 using Matrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
+
+/// @brief Alias for a dynamic Eigen stride.
+using DynamicStride = Eigen::Stride<Eigen::Dynamic, Eigen::Dynamic>;
+
+/// @brief Alias for a dynamic Eigen reference.
+template <typename T>
+using DynamicRef = Eigen::Ref<T, 0, DynamicStride>;
 
 }  // namespace fes
