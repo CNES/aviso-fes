233e42788ba2c76104ddab3d23dd85b0d821092b
Frédéric BRIOL <fbriol@gmail.com>
2024-10-23 17:15:08 +0200

    Refactor comments in Table class to improve readability and clarity

--- include/fes/wave/table.hpp
+++ include/fes/wave/table.hpp
@@ -214,7 +214,8 @@ class Table {
   /// The harmonic analysis method consists in expressing the ocean tidal
   /// variations as a sum of independent constituents accordingly to the tidal
   /// potential spectrum. Then the sea surface elevation at a point
-  /// \f$(x, y)\f$ and time \f(t\f) can be expressed as a linear sum as follow:
+  /// \f$(x, y)\f$ and time \fs(t\fs) can be expressed as a linear sum as
+  /// follow:
   ///
   /// \f[
   ///   S_{ap} = S_{0}(x, y) + \sum_{k=0}^n f_{k}(t)S_{k}(x, y)
@@ -222,18 +223,18 @@ class Table {
   /// \f]
   ///
   /// where:
-  ///   * \f(n\f) is the number of constituents,
+  ///   * \f$(n\f$) is the number of constituents,
   ///   * \f$S_{0}(x, y)\f$ is the mean sea level,
   ///   * \f$S_{k}(x, y)\f$ is the amplitude of the constituent of index
-  ///     \f(k\f),
+  ///     \f$(k\f$),
   ///   * \f$G_{k}(x, y)\f$ is the phase lag relative to Greenwich time,
   ///   * \f$w_{k}\f$ is the angular frequency of the constituent of index
-  ///     \f(k\f),
-  ///   * \f$v_{k}\f$ is the astronomical argument at time \f(t\f),
+  ///     \f$(k\f$),
+  ///   * \f$v_{k}\f$ is the astronomical argument at time \f$(t\f$),
   ///   * \f$f_{k}(t)\f$ is the nodal correction coefficient applied to
-  ///     the amplitude of the constituent of index \f(k\f),
+  ///     the amplitude of the constituent of index \f$(k\f$),
   ///   * \f$u_{k}(t)\f$ is the nodal correction coefficient applied to
-  ///     the phase of the constituent of index \f(k\f).
+  ///     the phase of the constituent of index \f$(k\f$).
   ///
   ///   The a priori analysis spectrum includes the most important astronomical
   ///   constituents in the Darwin development, completed by Shureman in 1958,
@@ -241,8 +242,8 @@ class Table {
   ///   astronomical arguments is taken from FES2014 tidal prediction software
   ///   and a complete definition of waves is also available in Shureman (1958).
   ///   This spectrum is the most commonly used for harmonic analysis due the
-  ///   simplification given by the nodal correction concept (\f(f\f) and
-  ///   \f(u\f) coefficients above) which allows dealing with slow motions of
+  ///   simplification given by the nodal correction concept (\f$(f\f$) and
+  ///   \f$(u\f$) coefficients above) which allows dealing with slow motions of
   ///   the lunar ascending node and reducing the number of constituents in the
   ///   tidal spectrum. More details about this harmonic analysis method can be
   ///   found in Ponchaut et al. 1999.
@@ -250,7 +251,7 @@ class Table {
   /// @param[in] h Sea level
   /// @param[in] f Nodal correction coefficient applied to the
   /// amplitude of the constituents analyzed.
-  /// @param[in] vu Astronomical argument at time \f(t\f) + the
+  /// @param[in] vu Astronomical argument at time \f$(t\f$) + the
   /// nodal correction coefficient applied to the phase of the
   /// constituents analyzed.
   static auto harmonic_analysis(const Eigen::Ref<const Eigen::VectorXd>& h,
