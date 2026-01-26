bc32a2118cb46a213a066109210eacf4f708a486
Frédéric BRIOL <fbriol@gmail.com>
2026-01-26 22:12:02 +0100

    fix(settings): remove unnecessary blank line in settings header

--- include/fes/settings.hpp
+++ include/fes/settings.hpp
@@ -2,7 +2,6 @@
 //
 // All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-
 /// @file include/fes/settings.hpp
 /// @brief Settings for the tide computation.
 #pragma once
