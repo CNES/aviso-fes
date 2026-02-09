a644418c72424d648c9fce4815eaa9be05c8b5f6
Frédéric BRIOL <fbriol@gmail.com>
2026-02-09 14:27:16 +0000

    fix: streamline cmake command execution in BuildExt class

--- setup.py
+++ setup.py
@@ -322,15 +322,14 @@ class BuildExt(setuptools.command.build_ext.build_ext):
 
         if configure:
             self.spawn(['cmake', str(WORKING_DIRECTORY), *cmake_args])
-        if not self.dry_run:
-            cmake_cmd = [
-                'cmake',
-                '--build',
-                '.',
-                '--target',
-                ext.name.split('.')[-1],
-            ]
-            self.spawn(cmake_cmd + build_args)  # type: ignore[arg-type]
+        cmake_cmd = [
+            'cmake',
+            '--build',
+            '.',
+            '--target',
+            ext.name.split('.')[-1],
+        ]
+        self.spawn(cmake_cmd + build_args)  # type: ignore[arg-type]
         os.chdir(str(WORKING_DIRECTORY))
 
 
