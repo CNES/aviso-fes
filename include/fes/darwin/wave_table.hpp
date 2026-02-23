434047df1c69c9a1b00943cc8f48b91005049850
Frédéric BRIOL <fbriol@gmail.com>
2026-02-23 17:16:59 +0000

    fix: remove redundant const qualifier from group_modulations parameter in compute_nodal_corrections method

--- include/fes/darwin/wave_table.hpp
+++ include/fes/darwin/wave_table.hpp
@@ -31,7 +31,7 @@ class WaveTable : public WaveTableInterface {
   /// @param[in] group_modulations If true, applies group modulations to nodal
   /// corrections.
   auto compute_nodal_corrections(const angle::Astronomic& angles,
-                                 const bool group_modulations) -> void final;
+                                 bool group_modulations) -> void final;
 
   /// @brief Clones the wave table.
   /// @return A unique pointer to the cloned wave table.
